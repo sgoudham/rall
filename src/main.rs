ec3a31d810b36dc46dac9e6faebd77ab0c706faf
Hammy <sgoudham@gmail.com>
2022-04-02 00:23:33 +0100

    Add main.rs for quick testing

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use rall::{Level, SimpleLogger};
+
+fn main() {
+    let mut logger = SimpleLogger::default();
+    logger.log(Level::TRACE, "My Best Friend Hazel :D");
+    logger.log(Level::DEBUG, "My Best Friend Hazel :D");
+    logger.log(Level::INFO, "My Best Friend Hazel :D");
+    logger.log(Level::WARN, "My Best Friend Hazel :D");
+    logger.log(Level::ERROR, "My Best Friend Hazel :D");
+    logger.log(Level::CRITICAL, "My Best Friend Hazel :D");
+}
